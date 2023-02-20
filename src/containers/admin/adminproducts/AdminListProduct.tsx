b5fae19b133befb643899e3cf59e1c4b885bc666
llqv <lequangvu070501@gmail.com>
2023-02-20 20:25:19 +0700

    update

--- src/containers/admin/adminproducts/AdminListProduct.tsx
+++ src/containers/admin/adminproducts/AdminListProduct.tsx
@@ -1,6 +1,6 @@
 import { Button, message, Popconfirm, Space, Table, Tag } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import AdminEditProduct from './AdminEditProduct';
 
 interface DataType {
